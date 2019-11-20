2f8e35ef68c73620d83a679627a3f53ff499f910
Ryan Billings <ryj.billings@gmail.com>
2019-11-20 10:36:14 -0500

    Split up the text using a delimeter
    This splits up the original text into an array using a non-word character delimeter.
    Traversal for previous and next words happens using the array
    
    E.g: "Hello there" => ["Hello", " ", "there"]

--- src/search/TextSearcher.java
+++ src/search/TextSearcher.java
@@ -4,8 +4,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextSearcher {
+	
+	private String[] words;
+	private final String WORD_DELIMETER = "\\s\\r\\n\\-\\.";
 
 	/**
 	 * Initializes the text searcher with the contents of a text file.
@@ -26,14 +31,79 @@ public class TextSearcher {
 		}
 		
 		init(w.toString());
+		r.close();
 	}
 	
 	/**
 	 *  Initializes any internal data structures that are needed for
 	 *  this class to implement search efficiently.
 	 */
-	protected void init(String fileContents) {
-		// TODO -- fill in implementation
+	private void init(String fileContents) {
+		words = fileContents.split(String.format("(?<=[%s])|(?=[%s])", WORD_DELIMETER, WORD_DELIMETER));
+	}
+	
+	/**
+	 * Case insensitive comparison to see if a contains b
+	 * @param a, the original word in the text
+	 * @param b, the query being searched
+	 * @return true if a contains b, false otherwise
+	 */
+	private boolean stringContains(String a, String b) {
+		return a.toLowerCase().contains(b.toLowerCase());
+	}
+	
+	/**
+	 * Determines if an index into words[i] is a valid word or a delimeter
+	 * @param s
+	 * @return true if the word is valid, false otherwise
+	 */
+	private boolean isWhitespace(String s) {
+		return s.matches(String.format("^[%s]$", WORD_DELIMETER));
+	}
+	
+	/**
+	 * Returns the previous n words from a given index
+	 * @param index - the index into the words array that is a search match
+	 * @param n - the previous n words to process
+	 * @return a string of the previous n words, or just the beginning of the file if reached
+	 */
+	private String previous(int index, int n) {
+		StringBuilder sb = new StringBuilder("");
+		while (n > 0 && index >= 0) {
+			sb.insert(0, words[index]);
+			if (!isWhitespace(words[index])) {
+				n--;
+			}
+			index--;
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Returns the next n words from a given index
+	 * @param index - the index into the words array that is a search match
+	 * @param n - the next n words to process
+	 * @return - a string of the next n words, or the rest of the file if the end is reached
+	 */
+	private String next(int index, int n) {
+		StringBuilder sb = new StringBuilder("");
+		while (n > 0 && index < words.length) {
+			sb.append(words[index]);
+			if (!isWhitespace(words[index])) {
+				n--;
+			}
+			index++;
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 *  Trim trailing commas and whitespace from the string
+	 * @param String s
+	 * @return s with trailing characters removed
+	 */
+	private String sanitize(String  s) {
+		return s.replaceAll(",*\\s*$", "");
 	}
 	
 	/**
@@ -44,8 +114,18 @@ public class TextSearcher {
 	 * @return One context string for each time the query word appears in the file.
 	 */
 	public String[] search(String queryWord,int contextWords) {
-		// TODO -- fill in implementation
-		return new String[0];
+		List<String> wordsFound = new ArrayList<String>();
+		
+		for(int i=0; i<words.length; i++) {
+			if(stringContains(words[i], queryWord)) {
+				String s = previous(i-1, contextWords) + words[i] + next(i+1, contextWords);
+				wordsFound.add(sanitize(s));
+			}
+		}
+		
+		String[] strArr = new String[wordsFound.size()];
+		strArr = wordsFound.toArray(strArr);
+		return strArr;
 	}
 }
 
